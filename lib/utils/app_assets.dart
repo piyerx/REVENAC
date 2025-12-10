10a7b62d368d708c4849ca2529c8583b868c672c
Piyush <145471711+piyerx@users.noreply.github.com>
2025-12-10 19:16:51 +0530

    feat: created the AppAssets class

---
+++ lib/utils/app_assets.dart
@@ -0,0 +1,17 @@
+class AppAssets {
+  // Base paths
+  static const String iconPath = 'assets/icons';
+  static const String imagePath = 'assets/images';
+
+  // Images (Placeholder for now will change it later)
+  static const String logo = '$imagePath/logo.png';
+  static const String heroWatermark = '$imagePath/recycle_node_watermark.png'; // [cite: 2]
+  static const String emptyStateParams = '$imagePath/empty_state.png';
+
+  // Icons (SVGs)
+  static const String metalIcon = '$iconPath/metal.svg';
+  static const String plasticIcon = '$iconPath/plastic.svg';
+
+  // Prevent instantiation
+  AppAssets._();
+}
